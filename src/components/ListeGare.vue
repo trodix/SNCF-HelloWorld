59fdf4bf159f384959ee4371d0e1584dd46b2e27
Sébastien Vallet <sebastien.vallet89@gmail.com>
2019-02-01 23:34:24 +0100

    dev: refactor code

--- src/components/ListeGare.vue
+++ src/components/ListeGare.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="container">
-        <form>
+        <!-- <form> -->
             <div class="row">
                 <div class="col-12 form-group">
                     <h2>Départ</h2>
-                    <input list="gares" v-model="gareStartSelected" v-on:input="getGaresStart(gareStartSelected)" id="gareStartSelected" name="gareStartSelected" class="form-control" placeholder="Choisir une gare de départ"/>
+                    <input list="gares" v-model="trajet.gareStart" v-on:input="filter(trajet.gareStart)" name="gareStartSelected" class="form-control" placeholder="Choisir une gare de départ"/>
                     <datalist id="gares">
-                        <option v-for="gare in garesByNameStart" :key="gare.id" :id="gare.id" :value="gare.name"></option>
+                        <option v-for="gare in gares" :key="gare.id" :value="gare.name"></option>
                     </datalist>
                 </div>
             </div>
@@ -14,18 +14,37 @@
             <div class="row">
                 <div class="col-12 form-group">
                     <h2>Arivée</h2>
-                    <input list="gares" v-model="gareEndSelected" v-on:input="getGaresEnd(gareEndSelected)" id="gareEndSelected" name="gareEndSelected" class="form-control" placeholder="Choisir une gare d'arrivé"/>
+                    <input list="gares" v-model="trajet.gareEnd" v-on:input="filter(trajet.gareEnd)" name="gareEndSelected" class="form-control" placeholder="Choisir une gare d'arrivé"/>
                     <datalist id="gares">
-                        <option :id="gare.id" v-for="gare in garesByNameEnd" :key="gare.id" :value="gare.name"></option>
+                        <option :id="gare.id" v-for="gare in gares" :key="gare.id" :value="gare.name"></option>
                     </datalist>
                 </div>
             </div>
             <div class="row">
                 <div class="col-12 form-group">
-                    <button class="btn btn-primary" v-on:submit.prevent>Afficher les horaires</button>
+                    <h2>Heure de départ</h2>
+                    <input v-model="trajet.horaireStart" type="date" name="horaireStart" class="form-control" placeholder="Choisir un horaire de départ"/>
                 </div>
             </div>
-        </form>
+            <div class="row">
+                <div class="col-12 form-group">
+                    <button class="btn btn-primary" v-on:click="getHoraires(trajet.horaireStart)">Afficher les horaires</button>
+                </div>
+            </div>
+        <!-- </form> -->
+        <div class="list-horaires my-5" v-if="ui.listHoraires.state == true">
+            <div class="row horaire-item">
+                <div class="col-12">
+                    <div class="horaire d-flex">
+                        <span class="time">{{ trajet.horaireStart }}</span> 
+                        <div class="d-flex flex-column">
+                            <p><span class="label">Départ:</span> {{ trajet.gareStart }}<p>
+                            <p><span class="label">Arrivé:</span> {{ trajet.gareEnd }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -35,46 +54,45 @@ export default {
         return {
             records: [],
             gares: [],
-            garesByNameStart: [],
-            garesByNameEnd: [],
-            gareStartSelected: "",
-            gareEndSelected: ""
+            trajet: {
+                gareStart: "",
+                gareEnd: "",
+                horaireStart: ""
+            },
+            ui: {
+                listHoraires: {
+                    // affiche ou non la liste des horaires
+                    state: false
+                }
+            }
         }
     },
     methods: {
-        getGaresStart(name) {
-            console.log(`#GetgaresStart(${name})`);
-            this.garesByNameStart = this.filter(this.gares, name);
+        getHoraires(horaireStart) {
+            this.ui.listHoraires.state = !this.ui.listHoraires.state;
+            console.log("horaireStart:", new Date(this.trajet.horaireStart));
+            console.log("gareStart:", this.trajet.gareStart);
+            console.log("gareEnd:", this.trajet.gareEnd);
         },
-        getGaresEnd(name) {
-            console.log(`#GetgaresEnd(${name})`);
-            this.garesByNameEnd = this.filter(this.gares, name);
-        },
-        getHoraires(depart, arrive) {
-
-        },
-        filter(arrayData, item) {
-            arrayData.filter((item) => {
+        filter(item) {
+            this.gares.filter((item) => {
                 return item.name.toLowerCase().indexOf(item.name.toLowerCase()) > -1;
             });
         }
         
     },
     created() {
-        fetch("https://ressources.data.sncf.com/api/records/1.0/search/?dataset=referentiel-gares-voyageurs&rows=10&sort=intitule_gare&facet=agence_gare&facet=region_sncf&facet=unite_gare&facet=departement&facet=segment_drg").then((response) => {
-            response.json().then((data) => {
-                console.log(data);
-                this.records = data.records;
-                this.records.forEach(element => {
-                    this.gares.push(
-                        {
-                            id: element.fields.code_plate_forme,
-                            name: element.fields.intitule_gare
-                        }
-                    );
-                });
-                this.garesByNameStart = this.gares;
-                this.garesByNameEnd = this.gares;
+        console.clear();
+        this.$http.get("https://ressources.data.sncf.com/api/records/1.0/search/?dataset=referentiel-gares-voyageurs&rows=10&sort=intitule_gare&facet=agence_gare&facet=region_sncf&facet=unite_gare&facet=departement&facet=segment_drg").then((response) => {
+            console.log("response", response);
+            this.records = response.body.records;
+            this.records.forEach(element => {
+                this.gares.push(
+                    {
+                        id: element.fields.code_plate_forme,
+                        name: element.fields.intitule_gare
+                    }
+                );
             });
         });
     }
@@ -82,5 +100,29 @@ export default {
 </script>
 
 <style scoped>
+
+.horaire {
+    background-color: #F5F5F5;
+    border-radius: 5px;
+    font-size: 1.2em;
+}
+
+.time {
+    font-weight: bold;
+    margin-right: 30px;
+    padding: 20px 30px;
+    background-color: #009CD3;
+    color: white;  
+}
+
+.label {
+    text-decoration: underline;
+    font-size: 1.3em;
+    margin-right: 20px;
+}
+
+.direction {
+    color: #888;
+}
 
 </style>
